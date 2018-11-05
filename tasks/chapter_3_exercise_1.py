0adac71cf76c20211203056ced886d8edc5fae18
Hank Ehly <henry.ehly@gmail.com>
2018-11-05 09:28:44 +0900

    Add chapter 3 exercise 1 script

---
+++ tasks/chapter_3_exercise_1.py
@@ -0,0 +1,31 @@
+import os
+from datetime import datetime
+
+import numpy as np
+
+from sklearn.datasets import fetch_mldata
+from sklearn.externals import joblib
+from sklearn.model_selection import GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+
+if __name__ == '__main__':
+    mnist = fetch_mldata('MNIST original')
+    X, y = mnist['data'], mnist['target']
+    X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+    shuffle_index = np.random.permutation(60000)
+    X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
+
+    knn_clf = KNeighborsClassifier()
+
+    param_grid = [{'weights': ['uniform', 'distance'], 'n_neighbors': [2, 3, 4, 5]}]
+
+    grid_search = GridSearchCV(knn_clf, param_grid, n_jobs=-1, cv=5, verbose=5)
+    grid_search.fit(X_train, y_train)
+
+    work_dir = os.path.dirname(os.path.realpath(__file__))
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    output_path = os.path.join(work_dir, 'chap3_ex1_grid_search_{timestamp}.pkl'.format(timestamp=timestamp))
+
+    joblib.dump(grid_search, output_path)
+
+    print('Result stored in {out}'.format(out=output_path))
